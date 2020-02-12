eca90f971731f6d8719176db398d82ffff1a658a
SahaPylypyuk <sashapulupyk@gmail.com>
2020-02-12 18:15:45 +0200

    Updates

--- precache-manifest.69ff32d27c17d77e9758eb6b9adb5546.js
+++ precache-manifest.69ff32d27c17d77e9758eb6b9adb5546.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb5b1aac7da35102f77d2d75bc7a1b99",
+    "revision": "6da3390f7490a01be8ee433e1f9d79ad",
     "url": "./index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.433df800.chunk.js.LICENSE"
   },
   {
-    "revision": "4538d0097f727c6411bb",
-    "url": "./static/js/main.6c719ce0.chunk.js"
+    "revision": "ea7c2ec10c84b517b92c",
+    "url": "./static/js/main.e295b9eb.chunk.js"
   },
   {
     "revision": "62474d1959b237c5a49f",
